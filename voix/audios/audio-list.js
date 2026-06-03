9928e3d89dab8fe1bd261c2435e0eb529bb1d24e
Elliot Royer <contactelliotroyer@gmail.com>
2026-06-03 03:57:03 +0200

    Ajout voix

---
+++ voix/audios/audio-list.js
@@ -0,0 +1,25 @@
+// Fichier généré automatiquement
+const audios = [
+  '/voix/audios/2014_07_03%20-%2000_29.mp3',
+  '/voix/audios/2018_09_29%20-%2004_36.mp3',
+  '/voix/audios/2020_09_05%20-%2000_00.mp3',
+  '/voix/audios/2020_10_10%20-%2008_41_e.mp3',
+  '/voix/audios/2021_01_07%20-%2000_51.mp3',
+  '/voix/audios/2021_01_07%20-%2020_27.mp3',
+  '/voix/audios/2021_10_12%20-%2006_38.mp3',
+  '/voix/audios/2021_10_12%20-%2011_51.mp3',
+  '/voix/audios/2022_01_25%20-%2000_12_00.mp3',
+  '/voix/audios/2022_02_14%20-%2011_03.mp3',
+  '/voix/audios/2022_02_14%20-%2025_46.mp3',
+  '/voix/audios/2022_07_07%20-%2021_06.mp3',
+  '/voix/audios/2022_07_09%20-%2006_06.mp3',
+  '/voix/audios/2022_08_13%20-%2005_35_e.mp3',
+  '/voix/audios/2022_09_11%20-%2004_37.mp3',
+  '/voix/audios/2022_12_15%20-%2001_31.mp3',
+  '/voix/audios/2023_07_30%20-%2008_26_e.mp3',
+  '/voix/audios/2024_01_16%20-%2013_18.mp3',
+  '/voix/audios/2024_03_06%20-%2001_47.mp3',
+  '/voix/audios/2024_03_06%20-%2006_18.mp3',
+];
+
+// Usage : player.src = audios[Math.floor(Math.random() * audios.length)];
